3b42415e733e83e6e2dcb31ac1b440607d4ee0bb
sagar <070bct535@ioe.edu.np>
2018-08-07 16:49:41 +0545

    portfolio project

---
+++ pages/Contact.vue
@@ -0,0 +1,20 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can contact me via email: <a href="#">070bct535@ioe.edu.np</a>
+      </p>
+    </div>
+  </section>
+</template>
+
+<script>
+    export default {
+        name: "Contact"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <h1>Hi, I'm S</h1>
+      <h2 >a Software Developer based in KTM</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+import AppLogo from '~/components/AppLogo.vue'
+
+export default {
+  components: {
+    AppLogo
+  }
+}
+</script>
+
+<style>
+.container {
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
+
